621b68ef10d1612093720dfc05b145ab24ca870c
Yeuris <yjtv@protonmail.com>
2021-06-21 21:52:38 -0400

    Añade el programa

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace underflow_overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte myByte = Convert.ToByte(Console.ReadLine());
+            byte otherByte = Convert.ToByte(Console.ReadLine());
+            //Sum(myByte, otherByte);
+            Substract(myByte, otherByte);
+        }
+
+        public static void Sum(byte first, byte second)
+        {
+            byte newValue = first += second;
+            Console.WriteLine(newValue);
+            if (first > newValue | second > newValue)
+            {
+                Console.WriteLine("Overflow");
+            }
+        }
+
+        public static void Substract(byte first, byte second)
+        {
+            byte newValue = first -= second;
+            Console.WriteLine(newValue);
+            if (first < newValue | second < newValue)
+            {
+                Console.WriteLine("Underflow");
+            }
+        }
+    }
+}
